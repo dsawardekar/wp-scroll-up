b1f30e40c3b73a4002bec8aba752db80fb5e48e2
Darshan Sawardekar <darshan@sawardekar.org>
2014-04-30 12:49:28 +0530

    Fixes integer options.

--- js/jquery-scroll-up-run.js
+++ js/jquery-scroll-up-run.js
@@ -1,8 +1,21 @@
 (function($) {
 
-  $(document).ready(function() {
+  var toInteger = function(key, options) {
+    if (options.hasOwnProperty(key)) {
+      options[key] = parseInt(options[key], 10);
+    }
+  };
+
+  var getScrollUpOptions = function() {
     var options = jquery_scroll_up;
-    $.scrollUp(options);
+    toInteger('scrollDistance', options);
+    toInteger('scrollSpeed', options);
+
+    return options;
+  };
+
+  $(document).ready(function() {
+    $.scrollUp(getScrollUpOptions());
   });
 
 }(jQuery));
