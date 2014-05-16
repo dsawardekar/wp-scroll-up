4d9dad610c202503951c1c4f91d4c40f8310ac7d
Darshan Sawardekar <darshan@sawardekar.org>
2014-05-16 18:06:29 +0530

    Upgrades to Arrow 0.7.0.

--- wp-scroll-up.php
+++ wp-scroll-up.php
@@ -9,29 +9,15 @@ Plugin URI: http://wordpress.org/plugins/wp-scroll-up
 License: GPLv2
 */
 
-require_once(__DIR__ . '/vendor/dsawardekar/wp-requirements/lib/Requirements.php');
+require_once(__DIR__ . '/vendor/dsawardekar/arrow/lib/Arrow/ArrowPluginLoader.php');
 
 function wp_scroll_up_main() {
-  $requirements = new WP_Requirements();
+  $options = array(
+    'plugin' => 'WpScrollUp\Plugin',
+    'arrowVersion' => '0.7.0'
+  );
 
-  if ($requirements->satisfied()) {
-    wp_scroll_up_register();
-  } else {
-    $plugin = new WP_Faux_Plugin('WP Scroll Up', $requirements->getResults());
-    $plugin->activate(__FILE__);
-  }
-}
-
-function wp_scroll_up_register() {
-  require_once(__DIR__ . '/vendor/dsawardekar/arrow/lib/Arrow/ArrowPluginLoader.php');
-
-  $loader = ArrowPluginLoader::getInstance();
-  $loader->register(__FILE__, '0.5.1', 'wp_scroll_up_load');
-}
-
-function wp_scroll_up_load() {
-  $plugin = \WpScrollUp\Plugin::create(__FILE__);
-  $plugin->enable();
+  ArrowPluginLoader::load(__FILE__, $options);
 }
 
 wp_scroll_up_main();
