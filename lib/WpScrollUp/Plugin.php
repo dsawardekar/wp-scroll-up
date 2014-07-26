040ca38e88d6ba952b7e9f560bde913462a75e29
Darshan Sawardekar <darshan@sawardekar.org>
2014-07-26 18:35:55 +0530

    Basic options page is up

--- lib/WpScrollUp/Plugin.php
+++ lib/WpScrollUp/Plugin.php
@@ -2,8 +2,6 @@
 
 namespace WpScrollUp;
 
-use Arrow\AssetManager\AssetManager;
-
 class Plugin extends \Arrow\Plugin {
 
   function __construct($file) {
@@ -11,29 +9,14 @@ class Plugin extends \Arrow\Plugin {
 
     $this->container
       ->object('pluginMeta', new PluginMeta($file))
-      ->object('assetManager', new AssetManager($this->container))
-      ->object('optionsManager', new OptionsManager($this->container));
+      ->packager('optionsPackager', 'Arrow\Options\Packager');
   }
 
   function enable() {
-    add_action('admin_init', array($this, 'initAdmin'));
-    add_action('admin_menu', array($this, 'initAdminMenu'));
-    add_action('init', array($this, 'initPlugin'));
+    add_action('init', array($this, 'initFrontEnd'));
   }
 
-  function toPluginDir($file) {
-    return untrailingslashit(plugin_dir_path($file));
-  }
-
-  function initAdmin() {
-    $this->lookup('optionsPostHandler')->enable();
-  }
-
-  function initAdminMenu() {
-    $this->lookup('optionsPage')->register();
-  }
-
-  function initPlugin() {
+  function initFrontEnd() {
     $this->initFrontEndScripts();
     $this->initFrontEndStyles();
   }
